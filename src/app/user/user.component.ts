56f2b2b7ba06bdc47eba0364059bfc3adae95404
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-06-17 10:40:02 +0200

    dialog-add-user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +13,9 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  readonly dialog = inject(MatDialog);
 
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
