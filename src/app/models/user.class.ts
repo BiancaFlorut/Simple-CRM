0d0ff05b31cbfa9b6887ca5cb850f38be808b25c
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-06-18 12:30:05 +0200

    customIdName for every user mit routerLink

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -1,6 +1,7 @@
 export class User {
   firstName: string;
   lastName: string;
+  email: string;
   birthDate: number;
   street: string;
   zipCode: string;
@@ -8,6 +9,7 @@ export class User {
   constructor(obj?: any) {
     this.firstName = obj ?obj.firstName : '';
     this.lastName = obj ? obj.lastName : '';
+    this.email  = obj ? obj.email : '';
     this.birthDate = obj ? obj.birthDate : 0;
     this.street = obj ? obj.street : '';
     this.zipCode = obj ? obj.zipCode : '';
@@ -18,6 +20,7 @@ export class User {
     return {
       firstName: this.firstName,
       lastName: this.lastName,
+      email: this.email,
       birthDate: this.birthDate,
       street: this.street,
       zipCode: this.zipCode,
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,19 +7,21 @@ import {MatTableModule} from '@angular/material/table';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
 import { DocumentData, Firestore, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatTableModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatTableModule, RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   data:DocumentData[] = [];
-  displayedColumns: string[] = [ 'name', 'birthDate', 'city'];
+  displayedColumns: string[] = [ 'name', 'email', 'birthDate', 'city'];
   dataSource = this.data;
+  newData!: DocumentData;
   readonly dialog = inject(MatDialog);
   user = new User();
   firestore: Firestore = inject(Firestore);
@@ -27,7 +29,14 @@ export class UserComponent {
 
   constructor() {
     this.unsubData = onSnapshot(collection(this.firestore, 'users'), (doc) => {
-      this.dataSource = doc.docs.map((doc) => doc.data());
+      this.dataSource = [];
+      doc.forEach((entry) => {
+        this.newData = entry.data();
+        this.newData['customIdName'] = entry.id;
+        this.dataSource.push(this.newData)      
+      })
+      console.log(this.dataSource);
+      
   });
   }
 
